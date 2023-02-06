06caca88b0a7ea1e8b1e1aa5e5094ab1d085d433
HuSky-Yellow <473795376@qq.com>
2023-02-06 18:21:57 +0800

    Initial Commit

---
+++ Lerna_C_Object/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lerna_C_Object
+//
+//  Created by 张照坤 on 2023/2/6.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
